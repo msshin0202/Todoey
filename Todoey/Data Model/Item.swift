7944cd74c01f4d4513cbe4e8447f393b588288e2
Minsoo Shin <msshin0202@gmail.com>
2018-04-29 14:10:45 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Minsoo Matthew Shin on 2018-04-28.
+//  Copyright © 2018 Minsoo Shin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
